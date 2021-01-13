233c9be1126df5bfafdfb81bce64119691c7cdbf
mariembo <77362841+mariembo@users.noreply.github.com>
2021-01-13 14:06:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ function initMap() {
 
 var g = 9.81;
 var c = 299792458;
-var Mt = 597200000000000000000000;
+var Mt = 5.972*Math.pow(10,24);
+console.log(Mt);
+console.log(Math.pow(10,24);
 var Rs = (2*g*Mt)/Math.pow(c,2);
 
 let elevationObserver = {
