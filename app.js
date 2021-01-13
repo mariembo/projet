a2ab4d401a57ddfce520a51f80509f4531a9d539
mariembo <77362841+mariembo@users.noreply.github.com>
2021-01-13 14:07:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,6 @@ function initMap() {
 var g = 9.81;
 var c = 299792458;
 var Mt = 5.972*Math.pow(10,24);
-console.log(Mt);
-console.log(Math.pow(10,24);
 var Rs = (2*g*Mt)/Math.pow(c,2);
 
 let elevationObserver = {
@@ -39,6 +37,10 @@ let elevationObserver = {
             this.value2 = newVal;
             var rA = value1 + 6371000;
             var rB = value2 + 6371000;
+            
+            
+            console.log(Mt);
+            console.log(Math.pow(10,24);
 
             //var phiA = ((-1)*g*Mt)/rA;
             //var phiB = ((-1)*g*Mt)/rB;
